f098f12bb6a44fd0ff3962484e68f66f9712e39e
Suresh Babu <telugu_sureshbabu@gmail.com>
2019-08-22 15:05:38 +0530

    modified permissions

--- scripts/post_build_script.sh
+++ scripts/post_build_script.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 aws s3 ls
-aws s3 cp target/*.jar s3://kanna-bucket-98765/artifacts/sampleapp-${GIT_COMMIT:0:8}.jar
+aws s3 cp target/myapp-*.war s3://kanna-bucket-98765/artifacts/sampleapp-${GIT_COMMIT:0:8}.war
+aws s3 cp target/myapp-*.war s3://kanna-bucket-98765/artifacts1/
